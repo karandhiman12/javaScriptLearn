4cc9d3fa2968389a2409c6c77b7bd79b4a0c9393
Karan Dhiman <dhimankaran260@gmail.com>
2025-05-19 21:37:52 +0530

    learning data type conversion in JS

---
+++ datatypeConversion.js
@@ -0,0 +1,22 @@
+let val = 34;
+let str = "34";
+let rtm = "34DF";
+let flag = 1
+
+let val2 = String(val);
+let str2 = Number(str);
+let rtm2 = Number(rtm);
+let flag2 = Boolean(flag)
+
+
+console.log(typeof(val2));
+console.log(val2);
+
+console.log(typeof(str2));
+console.log(str2);
+
+console.log(typeof(rtm2));
+console.log(rtm2);
+
+console.log(typeof(flag2));
+console.log(flag2);
