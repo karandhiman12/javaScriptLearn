245405d1bdb334a8b08c1eb96b205faa409a0154
Karan Dhiman <dhimankaran260@gmail.com>
2025-05-19 21:42:52 +0530

    making changes in datatypeConversion.js

--- datatypeConversion.js
+++ datatypeConversion.js
@@ -1,22 +1,29 @@
-let val = 34;
-let str = "34";
-let rtm = "34DF";
-let flag = 1
+// Declaring variables with different types of values
+let val = 34;           // Number
+let str = "34";         // String representing a number
+let rtm = "34DF";       // String with non-numeric characters
+let flag = 1;           // Number used for boolean conversion
 
-let val2 = String(val);
-let str2 = Number(str);
-let rtm2 = Number(rtm);
-let flag2 = Boolean(flag)
+// Type conversions
+let val2 = String(val);  // Converts number to string
+let str2 = Number(str);  // Converts string to number (works if string is numeric)
+let rtm2 = Number(rtm);  // Tries to convert a non-pure numeric string to number (will be NaN)
+let flag2 = Boolean(flag); // Converts number to boolean (non-zero is true)
 
+// Additional conversions for better understanding
+let zeroFlag = Boolean(0);   // Will be false since 0 is falsy
+let emptyString = Boolean(""); // Will be false since empty string is falsy
+let nonEmptyString = Boolean("hello"); // Will be true since non-empty strings are truthy
 
-console.log(typeof(val2));
-console.log(val2);
+let boolStr = Boolean("false"); // Attention: Non-empty strings are always true, even "false"
 
-console.log(typeof(str2));
-console.log(str2);
+console.log(typeof(val2), val2);  // Expected output: string "34"
+console.log(typeof(str2), str2);  // Expected output: number 34
+console.log(typeof(rtm2), rtm2);  // Expected output: number NaN (invalid conversion)
+console.log(typeof(flag2), flag2);// Expected output: boolean true
 
-console.log(typeof(rtm2));
-console.log(rtm2);
+console.log(typeof(zeroFlag), zeroFlag); // Expected output: boolean false
+console.log(typeof(emptyString), emptyString); // Expected output: boolean false
+console.log(typeof(nonEmptyString), nonEmptyString); // Expected output: boolean true
+console.log(typeof(boolStr), boolStr); // Expected output: boolean true (even though it's the word "false")
 
-console.log(typeof(flag2));
-console.log(flag2);
