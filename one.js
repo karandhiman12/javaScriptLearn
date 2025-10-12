71f8dede861049e413d68cf31a8bd00a913191f4
Karan Dhiman <dhimankaran260@gmail.com>
2025-10-12 21:25:47 +0530

    one.js

--- one.js
+++ one.js
@@ -1,13 +1,16 @@
 let a = 10;
 var b = 11;
-
+const c = 12;
 {
 	let a = 110;
 	var b = 130;
+	const c = 140;
 
 	console.log(a);
 	console.log(b);
+	console.log(c);
 }
 
 console.log(a);
 console.log(b);
+console.log(c);
